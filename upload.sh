9952f1528d30e44c9494347c57d03e8d3844ef2e
lkjhgfl <llllllloloo@gmail.com>
2018-05-27 10:30:19 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
-git clone https://github.com/lkjhgfl/KindleEar.git
+git clone https://github.com/lkjhgfl/KindleEar18.git
 python helper.py
 appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
 appcfg.py update KindleEar/
